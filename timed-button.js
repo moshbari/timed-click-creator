76839c628a99d45370428c55cd01884819f1849e
moshbari <engrmoshbari@gmail.com>
2025-08-03 08:49:11 +0400

    Create timed-button.js

---
+++ timed-button.js
@@ -0,0 +1,8 @@
+   <script>
+        window.addEventListener('load', function() {
+            setTimeout(function() {
+                const button = document.getElementById('timedButton');
+                button.classList.add('show');
+            }, 3000);
+        });
+    </script>
